53321a067475310f370f835d5d3ddc8da851ee14
Nikki Rudnick <186886169+nbrudnick@users.noreply.github.com>
2026-06-02 14:38:23 -0700

    removed bad optimization and extra code

--- #CS350HW7.py
+++ #CS350HW7.py
@@ -134,9 +134,42 @@ def main():
             print(f"Expected: {test_ans[i]}")
             print("BAD\n")
 
-def recursive_search(grid, r, c, r_max, c_max):
+def recursive_search(grid, r, c, rows, cols):
     #if we are out of bounds return
-    if r >= len(grid) or c >= len(grid[0]):
+    if r >= rows or c >= cols:
+        return 
+    if r < 0 or c < 0:
+        return 
+    if grid[r][c] == "1":
+        grid[r][c] = "0"  # Mark as visited by "sinking" it to water
+        #check all 4 conected directions for land (recursivley)
+        recursive_search(grid, r, c+1, rows, cols)
+        recursive_search(grid, r, c-1, rows, cols)
+        recursive_search(grid, r+1, c, rows, cols)
+        recursive_search(grid, r-1, c, rows, cols)
+    return
+
+def numIslands(grid):
+    if not grid:
+        return 0
+
+    rows = len(grid)
+    cols = len(grid[0])
+    islands = 0
+
+    # Loop through every cell in the grid
+    for r in range(rows):
+        for c in range(cols):
+            # If we find unvisited land, we found a new island!
+            if grid[r][c] == "1":
+                islands += 1
+                recursive_search(grid, r, c, rows, cols)#mark all connected "1"s to "0"s
+
+    return islands
+'''
+def recursive_search(grid, r, c, r_max, c_max, rows, cols):
+    #if we are out of bounds return
+    if r >= rows or c >= cols:
         return 
     if r < 0 or c < 0:
         return 
@@ -147,11 +180,11 @@ def recursive_search(grid, r, c, r_max, c_max):
             c_max = c # record max c reached to avoid re-checking
     if grid[r][c] == "1":
         grid[r][c] = "0"  # Mark as visited by "sinking" it to water
-        #check all 4 conected directions for land (recusrivly)
-        recursive_search(grid, r, c+1, r_max, c_max)
-        recursive_search(grid, r, c-1, r_max, c_max)
-        recursive_search(grid, r+1, c, r_max, c_max)
-        recursive_search(grid, r-1, c, r_max, c_max)
+        #check all 4 conected directions for land (recursivley)
+        recursive_search(grid, r, c+1, r_max, c_max, rows, cols)
+        recursive_search(grid, r, c-1, r_max, c_max, rows, cols)
+        recursive_search(grid, r+1, c, r_max, c_max, rows, cols)
+        recursive_search(grid, r-1, c, r_max, c_max, rows, cols)
     return
 
 def numIslands(grid):
@@ -170,154 +203,13 @@ def numIslands(grid):
                 islands += 1
                 r_max = 0
                 c_max = 0
-                recursive_search(grid, r, c, r_max, c_max)#mark all connected "1"s to "0"s
+                recursive_search(grid, r, c, r_max, c_max, rows, cols)#mark all connected "1"s to "0"s
                 if r_max != 0:#increment r based on max value reached in recursion
                     r = r_max
                 if c_max != 0:#increment c based on max value reached in recursion
                     c = c_max
 
-    return islands
-'''
-def mat_BFS(adj_mat, n, m):
-    component_sizes = []
-
-    # Loop through every single node in the graph
-    for h in range(n):
-        # If it's unvisited, we've possibly found a brand new connected component
-        if adj_max[h] == False:
-            #check the col for at least one '1' value,
-            for k in range(m):
-                if adj_mat[h][k] != 0:
-                    component = bfs(adj_mat, total_nodes, h, visited)
-                    component_sizes.append(len(component))
-                    break
-    return component_sizes
-
-
-def bfs(adj_matrix, total_nodes, start_node, visited):
-    #---NEW!----make an empty order array---#
-    order = []
-    #make my empty queue
-    queue = []
-    #push start node to the queue
-    queue.append(start_node)
-    #VISIT IT! We visit as we add!
-    visited[start_node] =True
-    order.append(start_node)
-
-    while queue:
-        current_node = queue.pop() #grabs from the end
-        if visited[current_node] == False:
-            #this current_node has not yet been visited
-            #visit the node
-            visited[current_node] = True
-            order.append(current_node)
-
-            #loop over the neighbors and push all unvisited to the stack
-            #----THIS IS THE SECTION DIFFERENT FROM AN ADJACENCY LIST----#
-            #row we want to iterate is the adjacency matrix at current node
-            neighbor = 0 #each column is a potential neighbor node
-            while neighbor < total_nodes: #row and column length will always be total_nodes
-                if visited[neighbor] == False and adj_matrix[current_node][neighbor] == "1":
-                    #if this element is a 1, it is a neighbor or current
-                    #append it if it is unvisited
-                    queue.append(neighbor)
-                neighbor += 1
-
-    return order
- '''
-'''#Start the BFS loop
-    while queue:
-        current_node = queue.pop(0) #grabs from the front of the queue
-        #loop over the neighbors and push all unvisited to the stack
-        neighbor_list = adj_list[current_node]
-        length_neighbor_list = len(neighbor_list)
-        index = 0
-        while index < length_neighbor_list:
-            neighbor = neighbor_list[index]
-            if visited[neighbor] == False:
-                #VISIT as we add to the queue
-                queue.append(neighbor)
-                visited[neighbor] = True
-                order.append(neighbor)
-            index += 1
-
-    print(f"The visited array shows we have seen each node: {visited}")
-    print(f"The order we visited the nodes: {order}")'''
-
-'''
-def dfs_mat(adj_matrix, total_nodes, start_node, visited):
-
-    order = []
-    #make my empty stack
-    stack = []
-    #push start node to the stack
-    stack.append(start_node)
-
-    #Start the DFS loop
-    while stack:
-        current_node = stack.pop() #grabs from the end
-        if visited[current_node] == False:
-            #this current_node has not yet been visited
-            #visit the node
-            visited[current_node] = True
-            order.append(current_node)
-
-            #loop over the neighbors and push all unvisited to the stack
-            #----THIS IS THE SECTION DIFFERENT FROM AN ADJACENCY LIST----#
-            #row we want to iterate is the adjacency matrix at current node
-            neighbor = 0 #each column is a potential neighbor node
-            while neighbor < total_nodes: #row and column length will always be total_nodes
-                if visited[neighbor] == False and adj_matrix[current_node][neighbor] == 1:
-                    #if this element is a 1, it is a neighbor or current
-                    #append it if it is unvisited
-                    stack.append(neighbor)
-                neighbor += 1
-
-    return order
-
-def adj_DPF(adj_list, total_nodes):
-    visited = [False] * total_nodes
-    component_sizes = []
-
-    # Loop through every single node in the graph
-    for start_node in range(total_nodes):
-        # If it's unvisited, we've found a brand new connected component
-        if visited[start_node] == False:
-            component = dfs_adj(adj_list, start_node, visited)
-            component_sizes.append(len(component))
-    
-    return component_sizes
-
-def dfs_adj(adj_list, start_node, visited):
-
-    order = []
-    #make my empty stack
-    stack = []
-    #push start node to the stack
-    stack.append(start_node)
-
-    #Start the DFS loop
-    while stack:
-        current_node = stack.pop() #grabs from the end
-        if visited[current_node] == False:
-            #this current_node has not yet been visited
-            #visit the node
-            visited[current_node] = True
-            order.append(current_node)
-
-            #loop over the neighbors and push all unvisited to the stack
-            neighbor_list = adj_list[current_node]
-            length_neighbor_list = len(neighbor_list)
-            index = 0
-            while index < length_neighbor_list:
-                neighbor = neighbor_list[index]
-                if visited[neighbor] == False:
-                    stack.append(neighbor)
-                index += 1
-
-    return order
-'''
+    return islands'''
 if __name__ == "__main__":
 
     main()
